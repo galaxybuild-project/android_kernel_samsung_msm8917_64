4694928a29f53cd1cf2ae53eb0704d5445472bef
kapmino269-MG <kapmino269@gmail.com>
2020-04-28 10:15:39 +0200

    update build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-export ARCH=arm
-export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH
-
-mkdir out
-
-make -C $(pwd) O=out CROSS_COMPILE=arm-linux-androideabi- j4primelte_sea_open_defconfig
-make -j64 -C $(pwd) O=out CROSS_COMPILE=arm-linux-androideabi-
- 
-cp out/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+OUT_DIR=out 
+COMMON_ARGS="-C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android" 
+export PATH=/home/kapmino269/65/aarch64-linux-android-4.9/bin:$PATH export ARCH=arm64 
+[ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR} 
+mkdir ${OUT_DIR} 
+make clean ${OUT_DIR} && make mrproper ${OUT_DIR} 
+make ${COMMON_ARGS} j4primelte_defconfig 
+#make ${COMMON_ARGS} nconfig 
+make -j$(nproc --all) ${COMMON_ARGS} 
+#cp ${OUT_DIR}/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
