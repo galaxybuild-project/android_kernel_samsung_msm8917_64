85d3595f5c2c76586149ee1b3c17c04cda829ad8
kapmino269-MG <kapmino269@gmail.com>
2020-06-05 19:58:30 +0200

    Rename phaoh defconfig and build script

--- build_pharoh.sh
+++ build_pharoh.sh
@@ -5,7 +5,7 @@ export PATH=/home/kapmino269/65/aarch64-linux-android-4.9/bin:$PATH export ARCH=
 [ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR} 
 mkdir ${OUT_DIR} 
 make clean ${OUT_DIR} && make mrproper ${OUT_DIR} 
-make ${COMMON_ARGS} j4plus_kali_defconfig 
+make ${COMMON_ARGS} j4plus_pharoh_defconfig 
 #make ${COMMON_ARGS} nconfig 
 make -j$(nproc --all) ${COMMON_ARGS} 
 #cp ${OUT_DIR}/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
