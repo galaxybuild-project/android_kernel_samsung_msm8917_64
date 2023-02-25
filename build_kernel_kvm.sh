cd78740eab8e626b59e246175d21d8124bb1823a
Ubuntu <ubuntu@ip-172-31-35-125.ap-southeast-1.compute.internal>
2023-02-25 14:30:45 +0000

    LuckyKernel v1.1 Development

--- build_kernel_kvm.sh
+++ build_kernel_kvm.sh
@@ -2,9 +2,12 @@
 OUT_DIR=out 
 export ANDROID_MAJOR_VERSION=p
 COMMON_ARGS="-C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android" 
-export PATH=/home/kokomail/UltraDisk/Tools/aarch64-linux-android-4.9/bin:$PATH export ARCH=arm64
+export PATH=/home/${USER}/UltraDisk/Tools/aarch64-linux-android-4.9/bin:$PATH export ARCH=arm64
 KBUILD_BUILD_VERSION="LuckyKernel-1.0-MSM8917-KVM"
-export KBUILD_BUILD_VERSION 
+export KBUILD_BUILD_VERSION
+export KBUILD_BUILD_VERSION
+export KBUILD_BUILD_USER=GalaxyBuildMeow
+export KBUILD_BUILD_HOST=LuckyKernel-MSM8917 
 [ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR} 
 mkdir ${OUT_DIR} 
 make mrproper ${OUT_DIR} 
--- build_pharoh.sh
+++ build_pharoh.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 OUT_DIR=out 
 COMMON_ARGS="-C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android" 
-export PATH=/home/kapmino269/65/aarch64-linux-android-4.9/bin:$PATH export ARCH=arm64 
+export PATH=/home/${USER}/UltraDisk/Tools/aarch64-linux-android-4.9/bin:$PATH export ARCH=arm64
+KBUILD_BUILD_VERSION="Pharoh-1.0-MSM8917"
+export KBUILD_BUILD_VERSION
+export KBUILD_BUILD_USER=GalaxyBuildMeow
+export KBUILD_BUILD_HOST=LuckyKernel-MSM8917 
 [ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR} 
 mkdir ${OUT_DIR} 
 make clean ${OUT_DIR} && make mrproper ${OUT_DIR} 
-make ${COMMON_ARGS} j4plus_pharoh_defconfig 
+make ${COMMON_ARGS} j6primelte_pharoh_defconfig 
 #make ${COMMON_ARGS} nconfig 
 make -j$(nproc --all) ${COMMON_ARGS} 
 #cp ${OUT_DIR}/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
